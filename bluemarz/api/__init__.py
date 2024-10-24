72d3776bca5a6f93aaf1d79dfad9b3cfd70e3d60
Luciano Mansonari <luciano.mansonari@fdte.io>
2024-10-24 14:19:42 -0300

    add export api key middleware

--- bluemarz/api/__init__.py
+++ bluemarz/api/__init__.py
@@ -3,6 +3,7 @@ from bluemarz.core.interfaces import Agent, Session, ToolDefinition, SyncTool, A
 from bluemarz.core.assignments import Assignment, AssignmentRunResult
 from bluemarz.core.class_registry import ai_agent, ai_session, assignment_executor, sync_tool_executor
 from bluemarz.core.spec_registry import get_assignment_by_id, save_assignment, set_assignment_registry, InMemmoryRegistry, StaticInMemmoryRegistry, SpecRegistry
+from bluemarz.core.middleware import api_key_middleware
 
 import bluemarz.core.models as models
 
