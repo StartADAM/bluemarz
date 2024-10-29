3ec04cc5dd2895f4e4646fab60688fcb486cff9c
Luciano Mansonari <luciano.mansonari@fdte.io>
2024-10-29 12:14:44 -0300

    added missing export

--- bluemarz/api/__init__.py
+++ bluemarz/api/__init__.py
@@ -1,5 +1,5 @@
 from bluemarz.core.models import AssignmentSpec, AgentSpec, SessionSpec, ToolSpec, SessionMessage, SessionFile, MessageRole, RunResultType, RunResult, ToolCall, ToolCallResult
-from bluemarz.core.interfaces import Agent, Session, ToolDefinition, SyncTool, AsyncTool, AssignmentExecutor
+from bluemarz.core.interfaces import Agent, Session, ToolDefinition, SyncTool, AsyncTool, AssignmentExecutor, SyncToolExecutor
 from bluemarz.core.assignments import Assignment, AssignmentRunResult
 from bluemarz.core.class_registry import ai_agent, ai_session, assignment_executor, sync_tool_executor
 from bluemarz.core.spec_registry import get_assignment_by_id, save_assignment, set_assignment_registry, InMemmoryRegistry, StaticInMemmoryRegistry, SpecRegistry
